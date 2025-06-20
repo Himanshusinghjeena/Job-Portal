79a066b2e15546bd3b97047dda7cdb447958b5e9
Himanshu Singh Jeena <114165565+Himanshusinghjeena@users.noreply.github.com>
2025-06-20 21:49:46 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./shared/Navbar";
 import HeroSection from "./HeroSection";
-import CategoryCarousel from "./CategoryCarousel";
+
 import LatestJobs from "./LatestJobs";
 import Footer from "./shared/Footer";
 import useGetAllJobs from "@/hooks/useGetAllJobs";
